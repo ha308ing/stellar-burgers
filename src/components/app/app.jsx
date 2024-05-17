d8cbb9c8a8651a69bd7ee8395182c36b68ce097b
unknown <75647984+ha308ing@users.noreply.github.com>
2024-05-18 02:53:18 +0400

    feat: app component

---
+++ src/components/app/app.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import styles from "./app.module.scss";
+import { AppHeader } from "../app-header/app-header";
+import { BurgerIngredients } from "../burger-ingredients/burger-ingredients";
+import { BurgerConstructor } from "../burger-constructor/burger-constructor";
+import { ingredientsRaw, order } from "../../utils/mock-data";
+import { formatIngredients, flatOrder, addIngredientsQty } from "../../utils";
+
+function App() {
+  const [orderState] = useState(order);
+  const orderFlatted = flatOrder(orderState);
+  const ingredientsQty = addIngredientsQty(ingredientsRaw, orderFlatted);
+
+  const [ingredientsGrouped, ingredientsGroups] =
+    formatIngredients(ingredientsQty);
+
+  return (
+    <section className={styles.container}>
+      <AppHeader />
+      <main className={styles.main}>
+        <BurgerIngredients
+          ingredients={ingredientsGrouped}
+          groups={ingredientsGroups}
+        />
+        <BurgerConstructor order={orderState} />
+      </main>
+    </section>
+  );
+}
+
+export default App;
