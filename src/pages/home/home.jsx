7c7f4a0ba8dbd8a8fc34e7debc187434358b6abe
unknown <75647984+ha308ing@users.noreply.github.com>
2024-06-18 00:49:28 +0400

    feat(pages): home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,36 +1,12 @@
-import { useEffect } from "react";
 import styles from "./home.module.scss";
 import {
   BurgerIngredients,
   BurgerConstructor,
-  ModalLoading,
-  ModalConnectError,
   LayoutMain,
 } from "../../components";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  ingredientsActions,
-  selectIgredientsGrouped,
-} from "../../services/ingredients";
-import { STATUSES } from "../../utils";
 
 export const HomePage = () => {
-  const { status, ingredientsQty } = useSelector(selectIgredientsGrouped);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(ingredientsActions.getIngredients());
-  }, [dispatch]);
-
-  const handleRetry = () => {
-    dispatch(ingredientsActions.getIngredients());
-  };
-
-  return status === STATUSES.PENDING ? (
-    <ModalLoading />
-  ) : status === STATUSES.REJECTED || ingredientsQty === 0 ? (
-    <ModalConnectError handleRetry={handleRetry} />
-  ) : (
+  return (
     <LayoutMain>
       <div className={styles.container}>
         <h1 className={styles.heading}>Соберите бургер</h1>
