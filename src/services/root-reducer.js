ad9923e28c26a6e325bd0360d1d9502f5bb525dc
unknown <75647984+ha308ing@users.noreply.github.com>
2024-06-18 00:54:55 +0400

    refactor(store): update root reducer
    - add slices
    - add global reset

--- src/services/root-reducer.js
+++ src/services/root-reducer.js
@@ -4,11 +4,45 @@ import { orderSlice } from "./order";
 import { ingredientSlice } from "./ingredient";
 import { burgerConstructorSlice } from "./burger-constructor";
 import { ingredientsTabsSlice } from "./ingredients-tabs";
+import {
+  formLoginSlice,
+  formRegisterSlice,
+  formProfileSlice,
+  formPasswordForgotSlice,
+  formPasswordResetSlice,
+} from "./forms";
+import { profileSlice } from "./profile";
 
-export const rootReducer = combineReducers({
+const appReducer = combineReducers({
   [ingredientsSlice.reducerPath]: ingredientsSlice.reducer,
   [orderSlice.reducerPath]: orderSlice.reducer,
   [ingredientSlice.reducerPath]: ingredientSlice.reducer,
   [burgerConstructorSlice.reducerPath]: burgerConstructorSlice.reducer,
   [ingredientsTabsSlice.reducerPath]: ingredientsTabsSlice.reducer,
+  [formRegisterSlice.reducerPath]: formRegisterSlice.reducer,
+  [formLoginSlice.reducerPath]: formLoginSlice.reducer,
+  [profileSlice.reducerPath]: profileSlice.reducer,
+  [formProfileSlice.reducerPath]: formProfileSlice.reducer,
+  [formPasswordForgotSlice.reducerPath]: formPasswordForgotSlice.reducer,
+  [formPasswordResetSlice.reducerPath]: formPasswordResetSlice.reducer,
 });
+
+export const RESET_STORE = "RESET_STORE";
+
+export const rootReducer = (state, action) => {
+  if (action.type === RESET_STORE) {
+    const ingredients = state[ingredientsSlice.reducerPath];
+    const { isAuthChecked } = state[profileSlice.reducerPath];
+    return appReducer(
+      {
+        [ingredientsSlice.reducerPath]: ingredients,
+        [profileSlice.reducerPath]: {
+          ...profileSlice.getInitialState(),
+          isAuthChecked,
+        },
+      },
+      action,
+    );
+  }
+  return appReducer(state, action);
+};
