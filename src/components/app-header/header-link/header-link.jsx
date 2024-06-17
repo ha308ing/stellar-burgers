1f58741c95132a88f9c2004ef50cc4bd6b3be02e
unknown <75647984+ha308ing@users.noreply.github.com>
2024-06-18 00:36:47 +0400

    refactor(components): app-header
    - use links and store

--- src/components/app-header/header-link/header-link.jsx
+++ src/components/app-header/header-link/header-link.jsx
@@ -1,20 +1,23 @@
 import styles from "./header-link.module.scss";
 import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+import { ROUTES } from "../../../utils";
 
-export const HeaderLink = ({ Icon, label, isActive }) => (
-  <a
-    className={`${styles.link} ${isActive ? "text_color_primary" : "text_color_inactive"}`}
-    href="/"
-  >
-    <div className={styles.link_icon}>
-      <Icon type={isActive ? "primary" : "secondary"} />
-    </div>
-    <div className={styles.link_label}>{label}</div>
-  </a>
+export const HeaderLink = ({ Icon, label, to }) => (
+  <NavLink className={styles.link} to={to}>
+    {({ isActive }) => (
+      <>
+        <div className={styles.link_icon}>
+          <Icon type={isActive ? "primary" : "secondary"} />
+        </div>
+        <div className={isActive ? styles.active : null}>{label}</div>
+      </>
+    )}
+  </NavLink>
 );
 
 HeaderLink.propTypes = {
   Icon: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired,
-  isActive: PropTypes.bool,
+  to: PropTypes.oneOf(Object.values(ROUTES)).isRequired,
 };
