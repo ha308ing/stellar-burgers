deada5c59893ba15750d4960aefc14cd9cb37f06
unknown <75647984+ha308ing@users.noreply.github.com>
2024-06-18 00:51:02 +0400

    feat(routing): protected-route

---
+++ src/components/protected-route-element/protected-route-element.js
@@ -0,0 +1,53 @@
+import { useSelector } from "react-redux";
+import { useLocation, Navigate } from "react-router-dom";
+import { ModalPending } from "../../components";
+import { selectProfile } from "../../services/profile";
+import { ROUTES } from "../../utils";
+import PropTypes from "prop-types";
+
+const ProtectedRouteElement = ({ onlyAuthorized = true, element }) => {
+  const location = useLocation();
+  const { isAuthChecked, user } = useSelector(selectProfile);
+
+  if (!isAuthChecked) {
+    return <ModalPending>загрузка</ModalPending>;
+  }
+
+  if (!onlyAuthorized && user) {
+    const from = location.state?.from ?? ROUTES.ROOT;
+    return <Navigate to={from} replace={true} />;
+  }
+
+  if (onlyAuthorized && !user) {
+    return (
+      <Navigate
+        to={ROUTES.LOGIN}
+        state={{ from: location.pathname }}
+        replace={true}
+      />
+    );
+  }
+
+  return element;
+};
+
+export const OnlyAuthorizedElement = ({ element }) => (
+  <ProtectedRouteElement element={element} />
+);
+
+export const OnlyUnauthorizedElement = ({ element }) => (
+  <ProtectedRouteElement onlyAuthorized={false} element={element} />
+);
+
+ProtectedRouteElement.propTypes = {
+  onlyAuthorized: PropTypes.bool,
+  element: PropTypes.element.isRequired,
+};
+
+OnlyAuthorizedElement.propTypes = {
+  element: PropTypes.element.isRequired,
+};
+
+OnlyUnauthorizedElement.propTypes = {
+  element: PropTypes.element.isRequired,
+};
