d9252d48c120f60156187325530cce4d0095776d
unknown <75647984+ha308ing@users.noreply.github.com>
2024-06-18 00:36:47 +0400

    refactor(components): app-header
    - use links and store

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -6,20 +6,41 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import { HeaderLink } from "./header-link/header-link";
 import styles from "./app-header.module.scss";
+import { ROUTES } from "../../utils";
+import { selectName } from "../../services/profile";
+import { useSelector } from "react-redux";
 
-export const AppHeader = () => (
-  <header className={styles.header}>
-    <nav className={styles.nav}>
-      <div className={styles.leftNav}>
-        <HeaderLink Icon={BurgerIcon} label="Конструктор" isActive={true} />
-        <HeaderLink Icon={ListIcon} label="Лента заказов" />
-      </div>
-      <div className={styles.rightNav}>
-        <HeaderLink Icon={ProfileIcon} label="Личный кабинет" />
-      </div>
-      <div className={styles.centerNav}>
-        <Logo />
-      </div>
-    </nav>
-  </header>
-);
+export const AppHeader = () => {
+  const name = useSelector(selectName);
+  const profileLinkLabel = name || "Личный кабинет";
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.leftNav}>
+          <HeaderLink
+            Icon={BurgerIcon}
+            label="Конструктор"
+            isActive={true}
+            to={ROUTES.ROOT}
+          />
+          <HeaderLink
+            Icon={ListIcon}
+            label="Лента заказов"
+            to={ROUTES.ORDERS_FEED}
+          />
+        </div>
+        <div className={styles.rightNav}>
+          <HeaderLink
+            Icon={ProfileIcon}
+            label={profileLinkLabel}
+            to={ROUTES.PROFILE}
+          />
+        </div>
+        <div className={styles.centerNav}>
+          <Logo />
+        </div>
+      </nav>
+    </header>
+  );
+};
