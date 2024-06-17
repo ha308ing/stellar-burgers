800eb30347e1b491e7a7e2c6245fc08a9959a4e0
unknown <75647984+ha308ing@users.noreply.github.com>
2024-06-18 00:38:29 +0400

    refactor(components): button-checkout
    - use store and routing

--- src/components/burger-constructor/button-checkout/button-checkout.jsx
+++ src/components/burger-constructor/button-checkout/button-checkout.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import PropTypes from "prop-types";
 
 export const ButtonCheckout = ({ handleClick, disabled }) => (
   <Button
@@ -11,3 +12,12 @@ export const ButtonCheckout = ({ handleClick, disabled }) => (
     Оформить заказ
   </Button>
 );
+
+ButtonCheckout.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  buttonText: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.elementType.isRequired,
+  ]),
+};
