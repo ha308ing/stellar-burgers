746cb663ffadbc26ce58077eb6ea6fb2c39df123
unknown <75647984+ha308ing@users.noreply.github.com>
2024-06-30 12:24:03 +0400

    refactor: main layout

--- src/components/layouts/layout-main/layout-main.jsx
+++ src/components/layouts/layout-main/layout-main.jsx
@@ -1,11 +1,22 @@
+import { useLocation } from "react-router-dom";
 import { AppHeader } from "../../app-header/app-header";
 import styles from "./layout-main.module.scss";
+import { useSelector } from "react-redux";
+import { selectApp } from "../../../services/app";
+import { useMemo } from "react";
 
 export const LayoutMain = ({ children }) => {
+  const location = useLocation();
+  const { isMobile } = useSelector(selectApp);
+
+  const key = useMemo(() => {
+    return isMobile ? location.pathname : null;
+  }, [isMobile, location]);
+
   return (
     <section className={styles.container}>
-      <AppHeader />
-      <div className={styles.mainContainer}>{children}</div>
+      <AppHeader key={key} />
+      {children}
     </section>
   );
 };
