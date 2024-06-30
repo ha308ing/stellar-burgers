79fb337b92148cde3b2e84218639334058d28176
unknown <75647984+ha308ing@users.noreply.github.com>
2024-06-30 12:36:05 +0400

    feat(mobile): home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,17 +4,40 @@ import {
   BurgerConstructor,
   LayoutMain,
 } from "../../components";
+import { withMobile } from "../../hocs/withMobile";
+import { OrderSummaryConstructor } from "../../components/order-summary/order-summary-constructor";
+import { OrderSummaryCheckout } from "../../components/order-summary/order-summary-checkout";
+import { OrderDetailsModal } from "../../components/order-details/order-details-modal";
+
+const MainMobile = () => (
+  <>
+    <BurgerIngredients />
+    <OrderSummaryConstructor />
+  </>
+);
+
+const MainDesktop = () => (
+  <>
+    <BurgerIngredients />
+    <BurgerConstructor />
+    <OrderSummaryCheckout />
+  </>
+);
+
+const Main = withMobile(MainDesktop, MainMobile);
 
 export const HomePage = () => {
   return (
-    <LayoutMain>
-      <div className={styles.container}>
-        <h1 className={styles.heading}>Соберите бургер</h1>
-        <main className={styles.main}>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </main>
-      </div>
-    </LayoutMain>
+    <>
+      <LayoutMain>
+        <div className={styles.container}>
+          <h1 className={styles.heading}>Соберите бургер</h1>
+          <main className={styles.main}>
+            <Main />
+          </main>
+        </div>
+      </LayoutMain>
+      <OrderDetailsModal />
+    </>
   );
 };
