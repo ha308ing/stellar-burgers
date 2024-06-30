38e0fe3adf1db18442b4fc7fa1ab5aa80e0e817f
unknown <75647984+ha308ing@users.noreply.github.com>
2024-06-30 12:19:59 +0400

    feat(mobile): use mobile app header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,21 +1,31 @@
+import { withMobile } from "../../hocs/withMobile";
 import { LayoutAppHeader as Header } from "../layouts/layout-app-header";
+import { MenuHamburger } from "./hamburger/hamburger";
 import { LinkOrdersFeed, LinkProfile, LinkRoot, LinkLogo } from "./header-link";
 
-export const AppHeader = () => {
-  return (
-    <Header>
-      <Header.Left>
-        <LinkRoot />
-        <LinkOrdersFeed />
-      </Header.Left>
+const HeaderDesktop = () => (
+  <Header>
+    <Header.Left>
+      <LinkRoot />
+      <LinkOrdersFeed />
+    </Header.Left>
 
-      <Header.Center>
-        <LinkLogo />
-      </Header.Center>
+    <Header.Center>
+      <LinkLogo />
+    </Header.Center>
 
-      <Header.Right>
-        <LinkProfile />
-      </Header.Right>
-    </Header>
-  );
-};
+    <Header.Right>
+      <LinkProfile />
+    </Header.Right>
+  </Header>
+);
+
+const HeaderMobile = () => (
+  <Header>
+    <LinkLogo />
+
+    <MenuHamburger />
+  </Header>
+);
+
+export const AppHeader = withMobile(HeaderDesktop, HeaderMobile);
