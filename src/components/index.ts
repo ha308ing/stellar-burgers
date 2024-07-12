07ae9f6dc08a35ef8be65a7f562aef8fec366895
unknown <75647984+ha308ing@users.noreply.github.com>
2024-07-13 01:08:18 +0400

    refactor: enclose ingredient-details

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { App } from "./app/app";
 export * from "./app-header";
 export * from "./burger-constructor";
 export * from "./burger-ingredients";
+export * from "./ingredient-details";
 export { OrderDetails } from "./order-details/order-details";
 export { LayoutForm, LayoutMain, LayoutProfile } from "./layouts";
 export { ModalFulfilled, ModalPending, ModalRejected } from "./modal";
@@ -10,8 +11,3 @@ export {
   OnlyAuthorizedElement,
   OnlyUnauthorizedElement,
 } from "./protected-route-element/protected-route-element";
-export {
-  IngredientDetailsModal,
-  IngredientDetailsParams,
-  IngredientDetails,
-} from "./ingredient-details";
--- src/components/ingredient-details/index.ts
+++ src/components/ingredient-details/index.ts
@@ -1,3 +1,2 @@
-export { IngredientDetailsParams } from "./ingredient-details-params";
 export { IngredientDetailsModal } from "./ingredient-details-modal";
 export { IngredientDetails } from "./ingredient-details";
