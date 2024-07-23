ca4122eaec3ca203cebfbab379070e75df5db7ac
unknown <75647984+ha308ing@users.noreply.github.com>
2024-07-24 03:59:16 +0400

    feat(feeds): pages
    - order-history
    - order-info
    - orders-feed

--- src/pages/order-history/order-history.tsx
+++ src/pages/order-history/order-history.tsx
@@ -1,14 +1,60 @@
-import { LayoutProfile as LP } from "components";
-import type { FC } from "react";
+import { LayoutProfile as LP, FeedMessage, OrderCardLink } from "components";
+import { useAppDispatch, useAppSelector } from "hooks";
+import { useEffect, type FC } from "react";
+import { selectOrdersHistory, ordersHistoryActions } from "services";
+import styles from "./order-history.module.scss";
 
-export const OrderHistoryPage: FC = () => (
-  <>
-    <LP.Content>
-      <h1>Order history</h1>
-    </LP.Content>
-    <LP.Footer>
-      В этом разделе вы можете
-      <br /> просмотреть свою историю заказов
-    </LP.Footer>
-  </>
-);
+export const OrderHistoryPage: FC = () => {
+  const dispatch = useAppDispatch();
+  const {
+    isLoading,
+    isNoOrders,
+    ordersByNumbers,
+    ordersNumbers,
+    isError,
+    message,
+  } = useAppSelector(selectOrdersHistory);
+
+  useEffect(() => {
+    dispatch(ordersHistoryActions.connect());
+
+    return () => {
+      dispatch(ordersHistoryActions.disconnect());
+    };
+  }, [dispatch]);
+
+  const retryHandler = () => {
+    dispatch(ordersHistoryActions.connect());
+  };
+
+  const ordersCards = ordersNumbers.map((orderNumber) => {
+    const orderInfo = ordersByNumbers[orderNumber];
+    return (
+      <OrderCardLink
+        key={orderNumber}
+        orderInfo={orderInfo}
+        withStatus={true}
+      />
+    );
+  });
+
+  return (
+    <>
+      <LP.Content>
+        {isLoading ? (
+          <FeedMessage message="грузим историю заказов" />
+        ) : isError ? (
+          <FeedMessage message={message} clickHandler={retryHandler} />
+        ) : isNoOrders ? (
+          <FeedMessage message="нет заказов" clickHandler={retryHandler} />
+        ) : (
+          <section className={styles.orders}>{ordersCards}</section>
+        )}
+      </LP.Content>
+      <LP.Footer>
+        В этом разделе вы можете
+        <br /> просмотреть свою историю заказов
+      </LP.Footer>
+    </>
+  );
+};
--- src/pages/orders-feed/orders-feed.tsx
+++ src/pages/orders-feed/orders-feed.tsx
@@ -1,8 +1,89 @@
-import { LayoutMain } from "components";
-import type { FC } from "react";
+import { LayoutMain, FeedMessage, OrderCardLink } from "components";
+import { useAppDispatch, useAppSelector } from "hooks";
+import { useEffect, type FC } from "react";
+import { ordersFeedActions, selectOrdersFeed } from "services";
+import styles from "./orders-feed.module.scss";
 
-export const OrdersFeedPage: FC = () => (
-  <LayoutMain>
-    <h1>Orders Feed</h1>
-  </LayoutMain>
-);
+export const OrdersFeedPage: FC = () => {
+  const {
+    isLoading,
+    isNoOrders,
+    orderNumbersDone,
+    orderNumbersPending,
+    isError,
+    message,
+    total,
+    totalToday,
+    ordersByNumbers,
+    ordersNumbers,
+  } = useAppSelector(selectOrdersFeed);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(ordersFeedActions.connect());
+
+    return () => {
+      dispatch(ordersFeedActions.disconnect());
+    };
+  }, [dispatch]);
+
+  const retryHandler = () => {
+    dispatch(ordersFeedActions.connect());
+  };
+
+  const ordersCards = ordersNumbers.map((orderNumber) => {
+    const orderInfo = ordersByNumbers[orderNumber];
+    return <OrderCardLink key={orderNumber} orderInfo={orderInfo} />;
+  });
+
+  const ordersDone = orderNumbersDone.map((orderNumber) => (
+    <li key={orderNumber}>{orderNumber}</li>
+  ));
+
+  const ordersPending = orderNumbersPending.map((orderNumber) => (
+    <li key={orderNumber}>{orderNumber}</li>
+  ));
+
+  return (
+    <LayoutMain title="Лента заказов">
+      {isLoading ? (
+        <FeedMessage message="грузим ленту заказов" />
+      ) : isError ? (
+        <FeedMessage message={message} clickHandler={retryHandler} />
+      ) : isNoOrders ? (
+        <FeedMessage message="нет заказов" clickHandler={retryHandler} />
+      ) : (
+        <main className={styles.main}>
+          <section className={styles.orders}>{ordersCards}</section>
+
+          <section className={styles.summary}>
+            <div className={styles.done}>
+              <div className={styles.summaryTitle}>Готовы:</div>
+              <div className={styles.summaryDone}>
+                <ul>{ordersDone}</ul>
+              </div>
+            </div>
+
+            <div className={styles.pending}>
+              <div className={styles.summaryTitle}>В работе:</div>
+              <div className={styles.summaryPending}>
+                <ul>{ordersPending}</ul>
+              </div>
+            </div>
+
+            <div className={styles.total}>
+              <div className={styles.summaryTitle}>Выполнено за всё время:</div>
+              <div className={styles.summaryTotal}>{total}</div>
+            </div>
+
+            <div className={styles.totalToday}>
+              <div className={styles.summaryTitle}>Выполнено за сегодня:</div>
+              <div className={styles.summaryTotal}>{totalToday}</div>
+            </div>
+          </section>
+        </main>
+      )}
+    </LayoutMain>
+  );
+};
