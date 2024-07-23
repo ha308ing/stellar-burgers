28f8164b88c877048cdb3be93cfce6a2dbbd1e99
unknown <75647984+ha308ing@users.noreply.github.com>
2024-07-24 03:54:51 +0400

    feat(feeds): update components
    - modals update z-index
    - order details add link

--- src/components/order-details/order-details-modal.tsx
+++ src/components/order-details/order-details-modal.tsx
@@ -2,9 +2,11 @@ import { useAppDispatch, useAppSelector } from "hooks";
 import { orderActions, selectOrderInfo } from "services";
 import { ModalPending, ModalRejected } from "components/modal";
 import { OrderDetails } from "components/order-details";
-import { STATUSES } from "utils";
+import { ROUTES, STATUSES } from "utils";
 import { withMobileModal } from "hocs";
 import type { FC } from "react";
+import { Link } from "react-router-dom";
+import styles from "./order-details-modal.module.scss";
 
 const Modal = withMobileModal(OrderDetails, "Заказ оформлен");
 
@@ -17,7 +19,14 @@ export const OrderDetailsModal: FC = () => {
   };
 
   if (orderIdStatus === STATUSES.PENDING)
-    return <ModalPending>оформляем заказ</ModalPending>;
+    return (
+      <ModalPending>
+        <div>оформляем заказ</div>
+        <Link type="primary" to={ROUTES.ORDERS} className={styles.linkToOrders}>
+          история заказов
+        </Link>
+      </ModalPending>
+    );
 
   if (orderIdStatus === STATUSES.REJECTED)
     return (
--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -3,14 +3,26 @@ import doneImage from "images/done.png";
 import { useAppSelector } from "hooks";
 import { selectOrderInfo } from "services";
 import type { FC } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { ROUTES } from "utils";
 
 export const OrderDetails: FC = () => {
   const { orderId, orderName } = useAppSelector(selectOrderInfo);
+  const location = useLocation();
 
   return (
     <section className={styles.container}>
       <span className={styles.subHeader}>{orderName}</span>
-      <h1 className={styles.header}>{orderId}</h1>
+      <h1 className={styles.header}>
+        <Link
+          to={`${ROUTES.ORDERS}/${orderId}`}
+          state={{ background: location }}
+          title="показать заказ"
+          className={styles.link}
+        >
+          {orderId}
+        </Link>
+      </h1>
       <span className={styles.subHeader}>идентификатор заказа</span>
       <img src={doneImage} className={styles.image} alt="" />
       <span className={styles.message}>Ваш заказ начали готовить</span>
